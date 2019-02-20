f444bde10db55ebd17b376e6ab2caf26a0205705
ViktorOster <Ozterkvlt@hotmail.com>
2019-02-20 15:23:26 +0200

    Updates

---
+++ precache-manifest.6bb473e32b7682639fee21ff3807e43b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spms-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8fd262fa491b5b5e3803",
+    "url": "/spms-app/static/js/runtime~main.8fd262fa.js"
+  },
+  {
+    "revision": "18d651a364e9e7074a38",
+    "url": "/spms-app/static/js/main.18d651a3.chunk.js"
+  },
+  {
+    "revision": "240b24e82051e1b76880",
+    "url": "/spms-app/static/js/1.240b24e8.chunk.js"
+  },
+  {
+    "revision": "18d651a364e9e7074a38",
+    "url": "/spms-app/static/css/main.8212ab91.chunk.css"
+  },
+  {
+    "revision": "240b24e82051e1b76880",
+    "url": "/spms-app/static/css/1.ed0c5044.chunk.css"
+  },
+  {
+    "revision": "09fb10c07b41ea8d50545dc33dd35801",
+    "url": "/spms-app/index.html"
+  }
+];
